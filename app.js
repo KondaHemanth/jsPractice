157c774d57383e33133e2850e8b4270151ff6533
KondaHemanth <pavanshemanth@gmail.com>
2020-02-03 22:58:15 +0530

    app.js changes v1

--- app.js
+++ app.js
@@ -2,5 +2,29 @@ document.querySelector('li').style.background = 'yellow';
 
 const items = document.getElementsByClassName('collection-item');
 
-console.log(items[0]);
+//console.log(items[0]);
 items[0].style.color = 'red';
+
+
+function add(){
+  var task = document.forms['task-form']['task'].value;
+  //console.log(task);
+
+  var elem = document.createElement('li');
+  elem.className = "collection-item";
+  var node = document.createTextNode(task);
+  var cross = document.getElementsByClassName('delete-item');
+  cross = cross[0];
+
+
+  console.log(elem);
+  console.log(node);
+
+  elem.appendChild(node);
+  elem.appendChild(cross);
+
+  var lists = document.getElementsByClassName('collection');
+  var list = lists[0];
+  //console.log(list);
+  list.appendChild(elem);
+}
