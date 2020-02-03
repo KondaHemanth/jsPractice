907929494ec12d341b0ff94ce8b13da3a759d30b
KondaHemanth <pavanshemanth@gmail.com>
2020-02-03 20:43:18 +0530

    changes to style of first item

---
+++ app.js
@@ -0,0 +1,6 @@
+document.querySelector('li').style.background = 'yellow';
+
+const items = document.getElementsByClassName('collection-item');
+
+console.log(items[0]);
+items[0].style.color = 'red';
