8ad337b8c0cdd6af16b79a8259a0c7c6af08a007
KondaHemanth <pavanshemanth@gmail.com>
2020-02-04 22:51:06 +0530

    practice

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-//document.querySelector('li').style.background = 'yellow';
+document.querySelector('li').style.background = 'yellow';
 
-//const items = document.getElementsByClassName('collection-item');
+const items = document.getElementsByClassName('collection-item');
 
 //console.log(items[0]);
-//items[0].style.color = 'red';
+items[0].style.color = 'red';
 
 
 function add(){
